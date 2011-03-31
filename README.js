ee5789ff6ff87850c878ba0936e330c4ed6e2ac2
Rio Akasaka <rio@rioleo.org>
2011-03-31 12:03:13 -0700

    Added more details to README

--- README.js
+++
@@ -1,6 +0,0 @@
-TwitterNow
-Released March 31, 2011
-
-TwitterNow allows you to search the Twitter search stream for keywords and limit the results by geography. It also attempts to do some rudimentary geocoding once the data is retrieved from the stream, by using TinyGeocoder API to match the user-defined location on the biography page to a physical latitude and longitude. A MySQL database is required. The schema is also included.
-
-Please contact rio@cs.stanford.edu for questions or concerns. I would also be interested in its future applications and utility!
